84322fef4e9006ca5ca5bb2d51636d300fbf1585
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-21 18:11:27 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = 15;
+            
+
+            if (a % 2 == 0)
+                Console.WriteLine("$ {a} jest liczbą parzystą");
+            else
+                Console.WriteLine($"{a} jest liczbą nieparzystą");
+
+
+            Console.ReadKey();
+
+           
+          
+        }
+    }
+}
